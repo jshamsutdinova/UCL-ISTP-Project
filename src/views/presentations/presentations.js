9a6d05c9cc223cc0581f9b4716e61e516fc52337
Julia Shamsutdinova <shjn0297@gmail.com>
2021-09-15 23:32:00 +0800

    Created Presentation page

--- src/views/presentations/presentations.js
+++ src/views/presentations/presentations.js
@@ -1,3 +1,28 @@
+import pdfInfo from '@/components/card/pdf-info/pdf-info.vue'
+
 export default ({
-  name: 'Presentation'
+  name: 'Presentation',
+
+  components: {
+    'pdf-info-card': pdfInfo
+  },
+
+  data: () => ({
+    files: [
+      {
+        id: 1,
+        type: 'Type of report',
+        title: 'Title',
+        mainAuthor: 'Speaker',
+        authors: 'Co-authors'
+      },
+      {
+        id: 2,
+        type: 'Oral report',
+        title: 'The Microwave source of type III radio burst according to SRH and LOFAR data',
+        mainAuthor: 'Kupriyanova E.G.',
+        authors: 'Kashapova L.K., Reid H.A.S.'
+      }
+    ]
+  })
 })
--- src/views/presentations/presentations.vue
+++ src/views/presentations/presentations.vue
@@ -1,6 +1,27 @@
 <template>
   <v-container>
-    Presentation
+    <div class="conf-name">
+      <h2>Conference</h2>
+    </div>
+    <div class="conf-date">
+      Date and location
+    </div>
+    <div class="test-block mt-2">
+      <v-row>
+        <v-col cols=12 md=4
+          v-for="file in files"
+          :key="file.id"
+        >
+          <pdf-info-card
+            :type="file.type"
+            :title="file.title"
+            :mainAuthor="file.mainAuthor"
+            :authors="file.authors"
+            :link="file.link"
+          />
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
